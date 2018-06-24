3e52cbabdb97c1e07fa642e7786b7eda410a3838
Kriszten <40462044+Kriszten@users.noreply.github.com>
2018-06-24 14:58:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;*/
+using System.IO.Compression;
+
+
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = @"C:\i2p\start";
+            string zipPath = @"C:\i2p\result.zip";
+            string extractPath = @"C:\i2p\extract";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
