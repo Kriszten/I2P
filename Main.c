6ef84e82846ad80125731109d5aff62c01d3c447
Kriszten <40462044+Kriszten@users.noreply.github.com>
2018-06-26 18:22:46 +0200

    Add files via upload

--- Main.c
+++ Main.c
@@ -1,58 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-#include <stdio.h>
-#include <string.h>
+char* osszefuz(char* szoveg1, char* szoveg2) {
+	unsigned int hossz = strlen(szoveg1) + strlen(szoveg2);
+	char *celszoveg;
+	celszoveg = (char*)malloc(hossz * sizeof(char)+1);
 
-int search(char* str) {
-	FILE* fp;
-	printf("Opening the file in read mode. \n");
-	fp = fopen("i2ptest01.txt", "r");
-	if (fp == NULL) {
-		printf("Could not open the file. \n");
-		return 1;
+	unsigned int i;
+	for (i = 0;i<hossz;i++)
+	{
+		if (i<strlen(szoveg1))
+			celszoveg[i] = szoveg1[i];
+		else
+			celszoveg[i] = szoveg2[i - strlen(szoveg1)];
 	}
-	printf("Reading the file. \n");
-	char c;
-	int wherelaststr = -1;
-	int i = 0;
-	int j = 0;
-	int len = strlen(str);      // str hossza
-	while (1) {		// végig megyünk a fájlon
-		c = fgetc(fp);	//Reading the file
-		if (c == EOF)
-			break;
-		if (c == str[j]) {     //ha str elsõ karakterét megtalálta keresünk tovább
-			while (c == str[j] && j != "\0") {		// addig amíg a karakterek egyeznek és nincs vége a source-nak
-				i++;
-				j++;
-				c = fgetc(fp);	//Reading the file
-				if (c == EOF)
-					break;
-			}
-			if (j == len && str[j] == '\0') {		// ha vége az str-nek 
-				// BINGO!! we find our search string.
-				wherelaststr = i;
-			}
-		}
-		i++;
-		j = 0;
-	}
-	if (wherelaststr != -1) {
-		printf("\n '%s' appears last at %d. character.\n", str, wherelaststr);
-	}
-	else {
-		printf("\n '%s' does not appear.\n", str);
-	}
-	return wherelaststr;
+	celszoveg[i] = '\0';
+
+	return celszoveg;
 }
 
+void szetszed(char* forrasszoveg, int szoveg1hossz, int szoveg2hossz) {
+	int hossz = strlen(forrasszoveg);
+	char *ujszoveg1 = (char*) malloc(szoveg1hossz * sizeof(char)+1);
+	char *ujszoveg2 = (char*) malloc(szoveg2hossz * sizeof(char)+1);
+	
+	int i,j=0;
 
+	for (i=0; i < szoveg1hossz;i++) {
+		ujszoveg1[i] = forrasszoveg[i];
+	}
+	ujszoveg1[i] = '\0';
+	while (j < szoveg2hossz) {
+		ujszoveg2[j] = forrasszoveg[i];
+		i++;
+		j++;
+	}
+	ujszoveg2[j] = '\0';
+
+	printf("%s = %s + %s \n", forrasszoveg, ujszoveg1, ujszoveg2);
+
+}
 
 int main()
 {
-	char *str = "aa";
-	int appears=search(str);
-	
+	char* szoveg1 = "sdfghsdgh";
+	char* szoveg2 = "aaaaaa";
+
+	char* forras = osszefuz(szoveg1, szoveg2);
+
+	printf("%s + %s = %s\n", szoveg1, szoveg2, forras);
+
+	szetszed(forras, strlen(szoveg1), strlen(szoveg2));
+
+	free(forras);
+
 
 	return 0;
-
 }
